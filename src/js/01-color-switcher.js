8c9eb9847db590114b095bf688ed5b73b57cd41a
eugene.vashchilin <77799844+V3ugen@users.noreply.github.com>
2022-08-12 15:09:19 +0300

    first done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const start = document.querySelector("button[data-start]");
+const stop = document.querySelector('button[data-stop] ');
+const bodyEl = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+let timerId = null;
+start.addEventListener("click", onStart);
+stop.addEventListener('click', onStop)
+
+
+function onStart() {
+  timerId = setInterval(() => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000)
+  start.setAttribute("disabled", true)
+}
+
+function onStop() {
+  clearInterval(timerId)
+  start.removeAttribute("disabled")
+}
